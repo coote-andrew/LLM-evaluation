4c25b9c8547a2051623384790cba97f60dddf4c9
coote-andrew <coote.andrew@gmail.com>
2026-03-13 16:33:15 +1100

    Switch database backend to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -74,8 +74,12 @@ DATA_DIR.mkdir(exist_ok=True)
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATA_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
     }
 }
 
