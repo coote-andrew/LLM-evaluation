e6dde487926bad51b68b2faf2b840bc01a77902b
coote-andrew <coote.andrew@gmail.com>
2026-03-13 16:49:25 +1100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ Django settings for config project.
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
